4e4b5230be59713028e976528160c305a5648cb5
user <bbcustom1993@gmail.com>
2018-10-18 11:45:40 +0300

    http://github.com/bbcustom/rtr105

---
+++ test_11.py
@@ -0,0 +1,53 @@
+'''
+def thing():
+    print('Hello')
+    print('Fun')
+thing()
+print('Zip')
+thing()
+'''
+#pirmais tests
+'''
+big = max('Hello world')
+print(big)
+tiny= min('Hello world')
+print(tiny)
+
+'''
+#otrais tests
+'''
+x = 5
+print('Hello')
+
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print('I sleep all night and I work all day.')
+
+print('Yo')
+x = x + 2
+print(x)
+'''
+#treshais tests
+'''
+x = 5
+print('Hello')
+
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print('I sleep all night and I work all day.')
+
+print('Yo')
+print_lyrics()
+x = x + 2
+print(x)
+'''
+#ceturtais tests
+
+'''
+def greet():
+    return "Hello"
+
+print(greet(), "Glenn")
+print(greet(), "Sally")
+'''
+#piektais tests
